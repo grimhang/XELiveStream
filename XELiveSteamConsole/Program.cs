1a2284bf1489900960bbf3c50affe2e96744c48f
Sungchool Park <grimhan2@gmail.com>
2021-04-11 02:03:58 +0900

    초기 커밋.

---
+++ XELiveSteamConsole/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.SqlServer.XEvent.XELite;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XELiveSteamConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OutputXELStream("Server=localhost;Database=master;Trusted_Connection=True;", "system_health");
+        }
+
+        static void OutputXELStream(string connectionString, string sessionName)
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            var xeStream = new XELiveEventStreamer(connectionString, sessionName);
+
+            Console.WriteLine("Press any key to stop listening...");
+            Task waitTask = Task.Run(() =>
+            {
+                Console.ReadKey();
+                cancellationTokenSource.Cancel();
+            });
+
+            Task readTask = xeStream.ReadEventStream(() =>
+            {
+                Console.WriteLine("Connected to session");
+                return Task.CompletedTask;
+            },
+                xevent =>
+                {
+                    Console.WriteLine(xevent);
+                    Console.WriteLine("");
+                    return Task.CompletedTask;
+                },
+                cancellationTokenSource.Token);
+
+
+            try
+            {
+                Task.WaitAny(waitTask, readTask);
+            } catch (TaskCanceledException)
+            {
+            }
+
+            if (readTask.IsFaulted)
+            {
+                Console.Error.WriteLine("Failed with: {0}", readTask.Exception);
+            }
+        }
+    }
+}
